f89e530c94602de627357940441bd02ef5b77a40
scripting_tips_and_helpful_hints <796484+linuxraptor@users.noreply.github.com>
2021-12-24 03:40:18 -0600

    Update parsing regex, act more deterministically.
    The "ip" utility's output has changed a little bit. Updated regex is more flexible to this change, so it will work with the old "ip" utility output and the newly-formatted output.
    
    Previously, the recommended routes were calculated and displayed. Now, they are forced to turn on. This is an unsafe change for someone who is modifying a machine remotely, but is remarkably useful for a machine with local access.
    
    Troubleshooting text has been added.
    
    The script is now at least roughly aware of how dumb it is.
    
    It also now actually supports more than just deluge, but hopefully any application that needs namespaced VPN controls to prevent routing breakages.

--- split-tunneling.bash
+++ split-tunneling.bash
@@ -2,42 +2,68 @@
 
 ##################
 # FIRST EDIT THESE TWO VARIABLES TO MATCH YOUR NETWORK DEVICES
-# Then, at the bottom of this file, make a deluge startup command that makes sense.
+# Then, at the bottom of this file, make a torrent client startup command that makes sense.
 VPN_IFACE="tun0"
 LOCAL_IFACE="eth0"
 ##################
 
 ROUTING_TABLE_EXIT_CODE="$(ip route show table FROM_VPN >/dev/null 2>&1; echo $?)"
 ROUTING_TABLE_CONTENTS="$(ip route show table FROM_VPN 2>/dev/null)"
-if [[ "${ROUTING_TABLE_EXIT_CODE}" -eq 0 ]] && [[ ! -z "${ROUTING_TABLE_CONTENTS}" ]];
-  then
-  printf "Routing table already initialized.\n"
-  VPN_IFACE_INFO="$(ip address list "${VPN_IFACE}")"
-  VPN_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/[[:digit:]]+\sbrd.*/\1/p' <<< "${VPN_IFACE_INFO}")"
-  printf "VPN internal IP: %s\n" "${VPN_IFACE_ADDR}"
-  LOCAL_IFACE_INFO="$(ip address list "${LOCAL_IFACE}")"
-  LOCAL_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/[[:digit:]]+\sbrd.*/\1/p' <<< "${LOCAL_IFACE_INFO}")"
-  printf "Local internal IP: %s\n" "${LOCAL_IFACE_ADDR}"
-else
+if [[ "${ROUTING_TABLE_EXIT_CODE}" -ne 0 ]] || [[ -z "${ROUTING_TABLE_CONTENTS}" ]]; then
   IP_ROUTES="$(ip route show)"
 
   # Route looks like:
   # 0.0.0.0/1 via 172.16.0.1 dev tun0
-  VPN_GATEWAY="$(sed -ne 's/^0\.0\.0\.0\/1\ via\ \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\ dev\ '"${VPN_IFACE}"'\ $/\1/p' <<< "${IP_ROUTES}")"
+  VPN_GATEWAY="$(sed -ne 's/^0\.0\.0\.0\/1\ via\ \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\ dev\ '"${VPN_IFACE}"'\s.*$/\1/p' <<< "${IP_ROUTES}")"
   printf "VPN internal gateway: %s\n" "${VPN_GATEWAY}"
 
-  LAN_GATEWAY="$(sed -ne 's/^default\ via\ \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\ dev\ '"${LOCAL_IFACE}"'\ \ metric\ 4\ $/\1/p' <<< "${IP_ROUTES}")"
+  LAN_GATEWAY="$(sed -ne 's/^default\ via\ \([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\ dev\ '"${LOCAL_IFACE}"'\s.*$/\1/p' <<< "${IP_ROUTES}")"
   printf "LAN internal gateway: %s\n" "${LAN_GATEWAY}"
 
   VPN_IFACE_INFO="$(ip address list "${VPN_IFACE}")"
-  VPN_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/[[:digit:]]+\sbrd.*/\1/p' <<< "${VPN_IFACE_INFO}")"
+  VPN_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/?[[:digit:]]?[[:digit:]]?\s.*/\1/p' <<< "${VPN_IFACE_INFO}")"
   printf "VPN internal IP: %s\n" "${VPN_IFACE_ADDR}"
 
   LOCAL_IFACE_INFO="$(ip address list "${LOCAL_IFACE}")"
-  LOCAL_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/[[:digit:]]+\sbrd.*/\1/p' <<< "${LOCAL_IFACE_INFO}")"
+  LOCAL_IFACE_ADDR="$(sed -n -r 'N;s/^.*inet\s([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\/?[[:digit:]]?[[:digit:]]?\s.*/\1/p' <<< "${LOCAL_IFACE_INFO}")"
   printf "Local internal IP: %s\n" "${LOCAL_IFACE_ADDR}"
 
-  printf "\nInitializing routes:\n"
+  # Check if IP addresses are accessible.
+  if [[ -z "${VPN_IFACE_ADDR}" || -z "${LOCAL_IFACE_ADDR}" ]]; then
+    printf "\nSomething is wrong...\n"
+    printf "An IP address is missing or cannot be parsed.\n"
+    printf "Both VPN and LAN IP addresses are required for these routing rules.\n"
+    printf "Possible causes:\n"
+    printf " - The VPN connection is not initialized.\n"
+    printf " - An interface name is misconfigured at the top of this script.\n"
+    printf " - There is a networking error.\n"
+    printf " - Command syntax has changed for the 'ip' utility used by this script.\n"
+    printf "\n"
+    printf "Start by diagnosing with these commands:\n"
+    printf "ip route show\n"
+    printf "ip address list\n"
+    exit 1
+  fi
+
+  # Check if routing gateways are known.
+  if [[ -z "${VPN_GATEWAY}" || -z "${LAN_GATEWAY}" ]]; then
+    printf "\nCannot find info for VPN or LAN gateways.\n"
+    printf "Try restarting your local interface with something like:\n"
+    printf "sudo systemctl restart networking\n"
+    printf "or, if that does not work:\n"
+    printf "sudo /etc/init.d/networking restart\n"
+    exit 1
+  fi
+  
+    printf "\nInitializing routes:\n"
+  # Checking if routing table names are registered.
+  # http://linux-ip.net/html/tools-ip-route.html
+  if [[ "$(grep FROM_LAN /etc/iproute2/rt_tables >/dev/null 2>&1; echo $?)" -ne 0 ]]; then
+    $(echo "10 FROM_LAN" >> /etc/iproute2/rt_tables)
+  fi
+  if [[ "$(grep FROM_VPN /etc/iproute2/rt_tables >/dev/null 2>&1; echo $?)" -ne 0 ]]; then
+    $(echo "20 FROM_VPN" >> /etc/iproute2/rt_tables)
+  fi
   # Build separate routing tables for local and remote traffic.
   NEW_ROUTING_TABLE="ip route del default;"
   NEW_ROUTING_TABLE+="ip route add default via "${VPN_GATEWAY}" dev "${VPN_IFACE}";"
@@ -54,14 +80,37 @@ else
   NEW_ROUTING_TABLE+="ip route add 0.0.0.0/1 via "${VPN_GATEWAY}" dev "${VPN_IFACE}" table FROM_VPN;"
   NEW_ROUTING_TABLE+="ip route del 128.0.0.0/1 via "${VPN_GATEWAY}" dev "${VPN_IFACE}";"
   NEW_ROUTING_TABLE+="ip route add 128.0.0.0/1 via "${VPN_GATEWAY}" dev "${VPN_IFACE}" table FROM_VPN;"
+  OLDIFS=${IFS}; IFS=$';'
+  for ROUTE in ${NEW_ROUTING_TABLE}; do
+    printf "\n${ROUTE}; "
+    eval "${ROUTE}"
+  done
+  IFS=${OLDIFS}
+  printf "\n"
+
+else
+   printf "Routing tables for this task already seem to exist.\n"
+   printf "Making this script robust enough to check their validity is too much work. :-)\n"
+   printf "Try restarting your local interface with something like:\n"
+   printf "sudo systemctl restart networking\n"
+   printf "or, if that does not work:\n"
+   printf "sudo /etc/init.d/networking restart\n"
+   printf "\nThen: make sure your VPN is running, and re-run this script.\n"
+  exit 1
+
 fi
+
+# Review the information on the screen and ensure it makes sense.
+sleep 7
 
 ##################
 # Start torrent client by specifying the VPN and local IP addresses:
-# deluged --port=58846 --interface="${VPN_IFACE_ADDR}" --ui-interface="${LOCAL_IFACE_ADDR}"
 # rtorrent -b "${VPN_IFACE_ADDR}"
+# Start deluge similarly:
+# deluged --port=58846 --interface="${VPN_IFACE_ADDR}" --ui-interface="${LOCAL_IFACE_ADDR}"
 # You probably also want to specify "--logfile=" somewhere so you can see if it has errors.
-# If you have a lesser-privilged torrent user for deluged, you can start that using sudo -u <USER> <COMMAND>:
+# If you have a lesser-privilged torrent user for your client, you can start that using sudo -u <USER> <COMMAND>:
 # sudo -u my-deluge-user deluged --port=...
+# sudo -u my-rtorrent-user rtorrent -b ...
 # Good luck.
 ##################
